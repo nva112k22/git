f4bf50d69aba1eb2c9e3a81f0311ee5b22d5ae05
unknown <annguyen1112k2@gmail.com>
2023-02-20 17:28:52 +0700

    Created our first cloud funtions.

--- cloud-funtions/secret-area.js
+++ cloud-funtions/secret-area.js
@@ -1,5 +1,10 @@
-exports.handler = function(event, context, callBack) {
-    callBack(null, {
+exports.handler = function(event, context, callback) {
+    //
+    //
+    //
+    //
+
+    callback(null, {
         statusCode: 200,
         body: "Welcome to the super secret area"
     })
